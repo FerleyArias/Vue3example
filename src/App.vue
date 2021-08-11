a93144959da9bb82b5919a059b8f9e41fee5ffb8
Zawng <edwinjpr11@gmail.com>
2021-08-11 17:22:59 -0500

    :construction: Test home

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
-<ul class="nav nav-pills">
-  <li class="nav-item">
-    //<img src="./assets/logo.png" class="card-img-top" alt="Logo">
-  </li>
-  <li class="nav-item">
-    <router-link class="nav-link active" to="/">Lista Pokémon</router-link>
-  </li>
-  <li class="nav-item">
-    <router-link class="nav-link" to="/list">Lista Pokémon</router-link>
-  </li>
-</ul>
-  <router-view />
+  <ul class="nav nav-pills position-fixed align-center my-nav">
+    <li class="nav-item">
+      <img
+        src="./assets/logo.png"
+        class="card-img-top"
+        id="principal"
+        alt="Logo"
+      />
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link bg-danger text-white" to="/"
+        >Pokémon</router-link
+      >
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link bg-light text-danger ml-10" to="/list"
+        >Lista Pokémon</router-link
+      >
+    </li>
+  </ul>
+  <div class="m-0 vh-100 row justify-content-center align-items-center">
+    <router-view class="col-auto p-5" />
+  </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
+<style scoped>
+#principal {
+  height: 2.5rem;
+  width: 2.5rem;
+  margin-right: 10px;
+}
+
+li {
+  margin-left: 10px;
+}
+
+.my-nav {
+  margin-top: 30px;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+</style>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="card" style="width: 18rem;">
-  <img src="../assets/logo.png" class="card-img-top" alt="Pokemon">
+  <div class="card justify-content-center align-items-center" style="width: 18rem;">
+  <img src="../assets/logo.png" id="pokemon" class="card-img-top" alt="Pokemon">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title text-center">Card title</h5>
+    <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">An item</li>
     <li class="list-group-item">A second item</li>
     <li class="list-group-item">A third item</li>
   </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
 </div>
 </template>
+<style scoped>
+  #pokemon {
+    height: 10rem;
+    width: 10rem;
+    padding: 10px;
+  }
+</style>
